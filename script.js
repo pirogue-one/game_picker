206f5c55092692ff14171dc6c824016a776a0792
aleriiu <aleriiiu@gmail.com>
2024-03-07 14:55:57 +0300

    added html, css and swiper

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict"
+
+const swiper = new Swiper('.swiper', {
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+});
+
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'vertical',
+//     loop: true,
+  
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+  
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+  
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+//   });
