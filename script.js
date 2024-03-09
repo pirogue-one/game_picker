e28c77ed9e6947a65e5b4daa95a6c030e5baf535
pirogue-one <alyona.evg@yandex.ru>
2024-03-09 16:26:03 +0300

    added limited checkboxes

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//переключение вопросов
+
 const swiper = new Swiper('.swiper-answer', {
 
     direction: 'vertical',
@@ -17,3 +19,25 @@ const swiper = new Swiper('.swiper-answer', {
 
       
 });
+
+//ограничение выбора checkbox
+
+const checks = document.querySelectorAll(".limited-checkbox-1");
+let max = 3;
+for (let i = 0; i < checks.length; i++)
+  checks[i].onclick = selectiveCheck;
+function selectiveCheck (event) {
+  let checkedChecks = document.querySelectorAll(".limited-checkbox-1:checked");
+  if (checkedChecks.length >= max + 1)
+    return false;
+}
+
+const checksTwo = document.querySelectorAll(".limited-checkbox-2");
+let maxTwo = 3;
+for (let i = 0; i < checksTwo.length; i++)
+  checksTwo[i].onclick = selectiveCheckTwo;
+function selectiveCheckTwo (event) {
+  let checkedChecks = document.querySelectorAll(".limited-checkbox-2:checked");
+  if (checkedChecks.length >= maxTwo + 1)
+    return false;
+}
